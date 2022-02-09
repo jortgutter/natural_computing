da950509df80f03e5130f36e4ac0472839fe2974
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-02-09 23:15:26 +0100

    Add tournament selection for parents

--- code/evolutionary_computing/ex8.py
+++ code/evolutionary_computing/ex8.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 from tree import Tree
 from tqdm import tqdm
 from node import *
@@ -57,8 +59,6 @@ def crossover(tree1: Tree, tree2: Tree):
     else:
         tree1.root = node2
 
-    node1.update_parent(parent2)
-
     if parent2:
         for i, c2 in enumerate(parent2.children):
             if node2 == c2:
@@ -67,6 +67,7 @@ def crossover(tree1: Tree, tree2: Tree):
     else:
         tree2.root = node1
 
+    node1.update_parent(parent2)
     node2.update_parent(parent1)
 
 
@@ -95,6 +96,7 @@ term_val = TerminalValue(0)
 POP_SIZE = 1000
 N_GEN = 50
 P_CROSSOVER = 0.7
+k = 500
 
 
 def calculate_fitness(tree: Tree, term_val: TerminalValue, xs: List[float], ys: List[float]):
@@ -105,20 +107,42 @@ def calculate_fitness(tree: Tree, term_val: TerminalValue, xs: List[float], ys: 
     return sum(errors)
 
 
+def select_parents(population: List[Tree], k: int) -> Tuple[Tree, Tree]:
+    group1 = np.random.permutation(population)[:k]
+    group2 = np.random.permutation(population)[k:2*k]
+
+    return get_best(group1), get_best(group2)
+
+
+def get_best(group: np.ndarray) -> Tree:
+    best_fit = np.inf
+    best_tree = None
+
+    for tree in group:
+        if tree.fitness < best_fit:
+            best_fit = tree.fitness
+            best_tree = tree
+
+    return best_tree
+
+
 population = [Tree(FunctionNode(None, functions=functions, terminal_value=term_val, max_depth=2)) for _ in range(POP_SIZE)]
 
 optimal_fitness = [np.inf]
+optimal_tree = None
 for tree in population:
     fitness = calculate_fitness(tree, term_val, xs, ys)
     if fitness < optimal_fitness[0]:
         optimal_fitness[0] = fitness
+        optimal_tree = tree
     tree.update_fitness(fitness)
 
 for gen in tqdm(range(N_GEN)):
+    replaced = 0
     new_pop = []
 
     for i in range(0, POP_SIZE, 2):
-        t1, t2 = np.random.choice(population, size=2, replace=False)
+        t1, t2 = select_parents(population, k)
 
         if np.random.uniform(0, 1) < 0.7:  # Crossover
             o1 = copy.deepcopy(t1)
@@ -130,16 +154,26 @@ for gen in tqdm(range(N_GEN)):
 
             new_pop.extend(sorted([t1, t2, o1, o2], key=lambda x: x.fitness)[:2])
 
+            if o1 in new_pop[-2:]:
+                replaced += 1
+            if o2 in new_pop[-2:]:
+                replaced += 1
+
         else:
             new_pop.extend([t1, t2])
 
     population = new_pop
+    print(f"Replaced {replaced} organisms")
 
     for tree in population:
         if tree.fitness < optimal_fitness[-1]:
             optimal_fitness[-1] = tree.fitness
+            optimal_tree = tree
 
     optimal_fitness.append(optimal_fitness[-1])
+
+print(optimal_tree)
+plot_predictions(optimal_tree, xs, ys)
 
 plt.plot(range(N_GEN+1), optimal_fitness)
 plt.show()
