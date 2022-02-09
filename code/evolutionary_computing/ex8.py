274f761c90f05ff902ac96ce2d6d6f1a3bdecd79
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-02-09 18:43:40 +0100

    Add load_data function. Add protected div and log functions. Add get_predicitions function.

--- code/evolutionary_computing/ex8.py
+++ code/evolutionary_computing/ex8.py
@@ -1,18 +1,52 @@
 import matplotlib.pyplot as plt
 from node import *
 
+
+def load_data(filename):
+    with open(filename, "r") as file:
+        xs = [float(term) for term in file.readline().split(", ")]
+        ys = [float(term) for term in file.readline().split(", ")]
+    return xs, ys
+
+
+def get_predictions(tree: Node, terminal_value: TerminalValue, x_vals: List[float]) -> List[float]:
+    results = []
+
+    for x in x_vals:
+        terminal_value.v = x
+        results.append(tree.evaluate())
+
+    return results
+
+
+def protected_div(a, b):
+    return 1 if b == 0 else np.divide(a, b)
+
+
+def protected_log(a):
+    return 0 if a <= 0 else np.log(a)
+
+
 functions = [
     (np.add, 2),
     (np.subtract, 2),
     (np.multiply, 2),
-    (np.divide, 2),
-    (np.log, 1),
+    (protected_div, 2),
+    (protected_log, 1),
     (np.exp, 1),
     (np.sin, 1),
     (np.cos, 1)
 ]
 
-root = FunctionNode(None, functions=functions, max_depth=2)
+xs, ys = load_data("data_ex8.txt")
 
+term_val = TerminalValue(0)
+root = FunctionNode(None, functions=functions, terminal_value=term_val, max_depth=6)
 print(root)
-print(f"\n{root.evaluate()}")
+
+res = get_predictions(root, term_val, xs)
+
+plt.plot(xs, ys, label="True", c="green")
+plt.plot(xs, res, label="Estimated", c="black")
+plt.legend()
+plt.show()
--- code/evolutionary_computing/node.py
+++ code/evolutionary_computing/node.py
@@ -1,5 +1,11 @@
+from dataclasses import dataclass
 from typing import *
 import numpy as np
+
+
+@dataclass
+class TerminalValue:
+    v: float
 
 
 class Node:
@@ -22,7 +28,7 @@ class FunctionNode(Node):
     A Node representing a unary or binary operator.
     The function of the Node is randomly selected from a list of functions.
     """
-    def __init__(self, parent, functions: List[Tuple[Any, int]], max_depth: int = 0):
+    def __init__(self, parent, functions: List[Tuple[Any, int]], terminal_value: Optional[TerminalValue] = None, max_depth: int = 0):
         super().__init__(parent)
 
         f_idx = np.random.choice(range(len(functions)))
@@ -30,8 +36,8 @@ class FunctionNode(Node):
         self.f, self.arity = fun
 
         self.max_depth = max_depth
-        self.children = [FunctionNode(self, functions, max_depth=max_depth) if self.depth < self.max_depth-1 else
-                         TerminalNode(self) for _ in range(self.arity)]
+        self.children = [FunctionNode(self, functions, terminal_value=terminal_value, max_depth=max_depth) if self.depth < self.max_depth-1 else
+                         TerminalNode(self, value=terminal_value) for _ in range(self.arity)]
 
     def evaluate(self) -> float:
         return self.f(*[c.evaluate() for c in self.children])
@@ -47,13 +53,13 @@ class TerminalNode(Node):
     A Node representing a terminal.
     The value, if not specified, is randomly drawn from a distribution Uniform(0, 1).
     """
-    def __init__(self, parent, value: Optional[float] = None):
+    def __init__(self, parent, value: Optional[TerminalValue] = None):
         super().__init__(parent)
 
-        self.value = value if value else np.random.uniform(0, 1)
+        self.value = value if value else TerminalValue(np.random.uniform(0, 1))
 
     def evaluate(self) -> float:
-        return self.value
+        return self.value.v
 
     def __repr__(self) -> str:
-        return f"\t- Terminal: {self.value}"
+        return f"\t- Terminal: {self.value.v}"
