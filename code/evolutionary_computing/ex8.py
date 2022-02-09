5cc4f475a5f9705652742055c34642486c4c7c06
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-02-09 15:05:18 +0100

    Move Function and Terminal to node.py. Create Node superclass

--- code/evolutionary_computing/ex8.py
+++ code/evolutionary_computing/ex8.py
@@ -1,37 +1,5 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
-
-class Function:
-    def __init__(self, parent=None, function=(lambda x: x, 1), children=[]):
-        self.p = parent
-        self.f, self.arity = function
-        self.children = children
-
-        self.depth = 0 if not self.p else self.p.get_depth() + 1
-
-    def get_depth(self):
-        return self.depth
-
-    def evaluate(self):
-        return self.f(*[c.evaluate() for c in self.children])
-
-    def __repr__(self):
-        newline = "\n"
-        tab = "\t"
-        return f"{tab*self.depth}- Function[{self.depth}]: {self.f}\n{tab*self.depth}{(newline + tab*self.depth).join([str(c) for c in self.children])}"
-
-
-class Terminal:
-    def __init__(self, value=0):
-        self.value = value
-
-    def evaluate(self):
-        return self.value
-
-    def __repr__(self):
-        return f"\t- Terminal: {self.value}"
-
+from node import *
 
 functions = [
     (np.add, 2),
@@ -44,18 +12,7 @@ functions = [
     (np.cos, 1)
 ]
 
-
-def rand_func(parent, terminal=Terminal(0)):
-    f_idx = np.random.choice(range(len(functions)))
-    f = functions[f_idx]
-    return Function(parent=parent, function=f, children=[terminal]*f[1])
-
-terminal = Terminal(5)
-terminal.depth = 2
-
-root = rand_func(None, terminal)
-
-root.children = [rand_func(root, terminal) for _ in range(root.arity)]
+root = FunctionNode(None, functions=functions, max_depth=2)
 
 print(root)
 print(f"\n{root.evaluate()}")
---
+++ code/evolutionary_computing/node.py
@@ -0,0 +1,59 @@
+from typing import Optional
+import numpy as np
+
+
+class Node:
+    """
+    Base class of a node. Could be either a Function or Terminal.
+    """
+    def __init__(self, parent):
+        self.p = parent
+        self.depth = 0 if not self.p else self.p.get_depth() + 1
+
+    def get_depth(self) -> int:
+        return self.depth
+
+    def evaluate(self) -> float:
+        pass
+
+
+class FunctionNode(Node):
+    """
+    A Node representing a unary or binary operator.
+    The function of the Node is randomly selected from a list of functions.
+    """
+    def __init__(self, parent, functions, max_depth: int = 0):
+        super().__init__(parent)
+
+        f_idx = np.random.choice(range(len(functions)))
+        fun = functions[f_idx]
+        self.f, self.arity = fun
+
+        self.max_depth = max_depth
+        self.children = [FunctionNode(self, functions, max_depth=max_depth) if self.depth < self.max_depth-1 else
+                         TerminalNode(self) for _ in range(self.arity)]
+
+    def evaluate(self) -> float:
+        return self.f(*[c.evaluate() for c in self.children])
+
+    def __repr__(self) -> str:
+        newline = "\n"
+        tab = "\t"
+        return f"{tab * self.depth}- Function[{self.depth}]: {self.f}\n{tab * self.depth}{(newline + tab * self.depth).join([str(c) for c in self.children])}"
+
+
+class TerminalNode(Node):
+    """
+    A Node representing a terminal.
+    The value, if not specified, is randomly drawn from a distribution Uniform(0, 1).
+    """
+    def __init__(self, parent, value: Optional[float] = None):
+        super().__init__(parent)
+
+        self.value = value if value else np.random.uniform(0, 1)
+
+    def evaluate(self) -> float:
+        return self.value
+
+    def __repr__(self) -> str:
+        return f"\t- Terminal: {self.value}"
