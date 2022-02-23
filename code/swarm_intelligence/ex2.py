14f579fe163612b8455a6caeea04d6a1eb5e7775
Jort.Gutter <jort.gutter@ru.nl>
2022-02-23 14:45:51 +0100

    plotting implemented for ex2

--- code/swarm_intelligence/ex2.py
+++ code/swarm_intelligence/ex2.py
@@ -32,7 +32,12 @@ for i in range(2):
         positions[i].append(x)
 
 # plotting:
-plt.plot(positions[0], label='case 1')
-plt.plot(positions[1], label='case 2')
+plt.plot(positions[0], label=f'omega={omegas[0]}, r={rs[0]}')
+plt.plot(positions[1], label=f'omega={omegas[1]}, r={rs[1]}')
+plt.plot([0 for i in positions[0]], linestyle=':', label='global optimum')
+plt.xlabel('iteration')
+plt.ylabel('position')
+
 plt.legend()
+
 plt.show()
