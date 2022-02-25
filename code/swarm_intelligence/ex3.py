5372ab529825c283b0e81fae72b2df36d3d29af6
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-02-25 15:37:59 +0100

    ex3.py: code cleanup

--- code/swarm_intelligence/ex3.py
+++ code/swarm_intelligence/ex3.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from tqdm import tqdm
 
+
 class Particle:
-    def __init__(self, x_init: np.ndarray, dim: int):
+    def __init__(self, x_init: np.ndarray):
         self.x = x_init
         self.v = np.zeros(self.x.shape)
         self.fitness = np.inf
@@ -23,11 +24,10 @@ class PSOCluster:
         self.alpha2 = alpha2
 
         # generate initial particles
-        data_dim = len(data[0])
         self.particles = []
         for i in range(self.n_particles):
             init_indices = np.random.choice(range(len(data)), self.k, replace=False)
-            self.particles.append(Particle(x_init=data[init_indices,:], dim=data_dim))
+            self.particles.append(Particle(x_init=data[init_indices, :]))
 
         # take first particle position as initial global best
         self.best = self.particles[0].best
@@ -36,8 +36,6 @@ class PSOCluster:
         # iteratively update particles:
         for it in tqdm(range(n_iterations)):
             for particle in self.particles:
-                #print(f'x: {particle.x}')
-                # calculate quantization error of particle:
                 error = self.score(particle.x, data)
                 particle.fitness = error
                 # update best and global best:
@@ -56,13 +54,12 @@ class PSOCluster:
         # return best cluster centers and their fitness:
         return np.array([self.best, self.best_fitness])
 
-
-
     def update(self, p: Particle):
+        # Update velocity
         r = np.random.random(2)
-        p.v = self.omega*p.v + self.alpha1*r[0]*(p.best - p.x) + self.alpha2*r[1]*(self.best - p.x)
-        if p == self.particles[0]:
-            print(f'p0.x:{p.x}')
+        p.v = self.omega * p.v + self.alpha1 * r[0] * (p.best - p.x) + self.alpha2 * r[1] * (self.best - p.x)
+
+        # Update position
         p.x = p.x + p.v
 
     def score(self, x: np.ndarray, data: np.ndarray):
@@ -77,39 +74,41 @@ class PSOCluster:
             clusters[c_i].append(distances[c_i])
 
         # calculate quantization error:
-        error = np.sum([np.sum(clusters[i])/len(clusters[i]) if len(clusters[i]) > 0 else 0 for i in range(self.k)])/self.k
+        error = np.sum(
+            [np.sum(clusters[i]) / len(clusters[i]) if len(clusters[i]) > 0 else 0 for i in range(self.k)]) / self.k
         return error
 
 
 class KMeans:
-    def __init__(self, k ):
+    def __init__(self, k):
         self.k = k
 
 
 class ArtificialData:
     def generate_data(self, n):
-        data = {}
+        data = dict()
         data['X'] = np.array([[np.random.uniform(-1, 1) for j in range(2)] for i in range(n)])
         data['y'] = np.array([self.classify(x) for x in data['X']])
         return data
 
+    @staticmethod
     def classify(self, x):
-        return 1 if x[0]>=0.7 or x[0] <= 0.3 and x[1] >= -0.2 - x[0] else 0
-
+        return 1 if x[0] >= 0.7 or x[0] <= 0.3 and x[1] >= -0.2 - x[0] else 0
 
 
 def plot_2D_data(X, y):
-    for i in range(max(y)+1):
-        class_members = X[y==i]
-        plt.scatter(class_members[:,0], class_members[:,1], label=f'class {i}')
+    for i in range(max(y) + 1):
+        class_members = X[y == i]
+        plt.scatter(class_members[:, 0], class_members[:, 1], label=f'class {i}')
         plt.legend()
         plt.xlabel('z0')
         plt.ylabel('z1')
     plt.show()
 
+
 # Load iris data:
 iris = datasets.load_iris()
-iris_data = {'X':iris.data, 'y': iris.target}
+iris_data = {'X': iris.data, 'y': iris.target}
 
 # generate artificial data:
 artificial_data = ArtificialData().generate_data(n=400)
@@ -118,7 +117,7 @@ artificial_data = ArtificialData().generate_data(n=400)
 omega = 0.72
 alpha1 = alpha2 = 1.49
 n_its = 100
-n_trials = 30
+n_trials = 1
 
 iris_PSO_results = np.array([])
 iris_PSO_scores = np.array([])
@@ -150,11 +149,5 @@ for i in range(n_trials):
         alpha2=alpha2
     ))
 
-
-
-
-
-
-plot_2D_data(iris_data['X'][:,:2], iris_data['y'])
-plot_2D_data(artificial_data['X'][:,:2], artificial_data['y'])
-
+plot_2D_data(iris_data['X'][:, :2], iris_data['y'])
+plot_2D_data(artificial_data['X'][:, :2], artificial_data['y'])
