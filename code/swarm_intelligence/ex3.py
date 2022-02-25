85b5a24b5bf9e3adb3bf4c4d0af22648c729d58b
Jort.Gutter <jort.gutter@ru.nl>
2022-02-25 18:10:56 +0100

    finished code

--- code/swarm_intelligence/ex3.py
+++ code/swarm_intelligence/ex3.py
@@ -59,7 +59,7 @@ class PSOCluster:
                 self.update(p=particle)
 
         # return best cluster centers and their fitness:
-        return np.array([self.best, self.best_fitness]), errors
+        return self.best, self.best_fitness
 
     def update(self, p: Particle):
         # Update velocity
@@ -69,20 +69,32 @@ class PSOCluster:
         p.x = p.x + p.v
 
     def score(self, x: np.ndarray, data: np.ndarray):
-        # assign data to nearest cluster centroids:
-        clusters = [[] for i in range(self.k)]
-        for d in data:
-            # calculate distance between data point and all cluster centroids of particle:
-            distances = np.array([np.linalg.norm(d - x[i]) for i in range(self.k)])
-            # retrieve distance index of the smallest distance:
-            c_i = np.argmin(distances)
-            # append distance to cluster
-            clusters[c_i].append(distances[c_i])
+        return quantization_error(self.k, x, data)
 
-        # calculate quantization error:
-        error = np.sum(
-            [np.sum(clusters[i]) / len(clusters[i]) if len(clusters[i]) > 0 else 0 for i in range(self.k)]) / self.k
-        return error
+
+def quantization_error(k, x, data):
+    # assign data to nearest cluster centroids:
+    clusters, labels = get_clusters(k, x, data)
+    # calculate quantization error:
+    error = np.sum(
+        [np.sum(clusters[i]) / len(clusters[i]) if len(clusters[i]) > 0 else 0 for i in range(k)]) / k
+    return error
+
+
+def get_clusters(k, x, data):
+    # assign data to nearest cluster centroids:
+    cluster_distances = [[] for i in range(k)]
+    labels = []
+    for d in data:
+        # calculate distance between data point and all cluster centroids of particle:
+        distances = np.array([np.linalg.norm(d - x[i]) for i in range(k)])
+        # retrieve distance index of the smallest distance:
+        c_i = np.argmin(distances)
+        # append label to labels:
+        labels.append(c_i)
+        # append distance to cluster
+        cluster_distances[c_i].append(distances[c_i])
+    return cluster_distances, np.array(labels)
 
 
 class ArtificialData:
@@ -97,45 +109,66 @@ class ArtificialData:
         return 1 if x[0] >= 0.7 or x[0] <= 0.3 and x[1] >= -0.2 - x[0] else 0
 
 
-def plot_2D_data(X, y):
-    for i in range(max(y) + 1):
-        class_members = X[y == i]
-        plt.scatter(class_members[:, 0], class_members[:, 1], label=f'class {i}')
-        plt.legend()
-        plt.xlabel('z0')
-        plt.ylabel('z1')
+def plot_2D_data(X: np.ndarray, y: np.ndarray, y_hat:np.ndarray, k:int, data_name:str, method_name:str):
+    colors = ['blue', 'red', 'orange']
+    for i in range(k):
+        plt.scatter(X[y == i][:, 0], X[y == i][:, 1], marker='o', s=80, c=colors[i], label=f'True class {i}')
+    for i in range(k):
+        plt.scatter(X[y_hat == i][:, 0], X[y_hat == i][:, 1], marker='.', c=colors[i], label=f'Predicted class {i}')
+    plt.legend()
+    plt.xlabel('feature 0')
+    plt.ylabel('feature 1')
+    plt.title(f'True vs Predicted labels of the {data_name} data\nClustered through {method_name}')
     plt.show()
+
 
 
 # Load iris data:
 iris = datasets.load_iris()
 iris_data = {'X': iris.data, 'y': iris.target}
+k_iris = 3
 
 # generate artificial data:
 artificial_data = ArtificialData().generate_data(n=400)
+k_artificial = 2
 
 # parameters for PSO clustering (taken from van der Merwe and Engelbrechts paper):
 omega = 0.72
 alpha1 = alpha2 = 1.49
 n_its = 100
-n_trials = 1
+n_trials = 30
 
-iris_PSO_results = np.array([])
-iris_PSO_scores = np.array([])
-artificial_PSO_results = np.array([])
-artificial_PSO_scores = np.array([])
+all_scores = {
+    'iris_PSO': [],
+    'iris_KMeans': [],
+    'artificial_PSO': [],
+    'artificial_KMeans': []
+}
+
+all_centroids = {
+    'iris_PSO': [],
+    'iris_KMeans': [],
+    'artificial_PSO': [],
+    'artificial_KMeans': []
+}
 
 # perform clustering:
 for i in range(n_trials):
     print(f'starting trial {i}:')
 
     # KMeans clustering on artificial data:
-    artificial_kmeans = cluster.KMeans(n_clusters=2, init='random', n_init=10, max_iter=100).fit(
+    artificial_kmeans = cluster.KMeans(n_clusters=k_artificial, init='random', n_init=10, max_iter=100).fit(
         artificial_data['X'])
-    print(artificial_kmeans.cluster_centers_)
+    centroids = artificial_kmeans.cluster_centers_
+    all_centroids['artificial_KMeans'].append(centroids)
+    all_scores['artificial_KMeans'].append(quantization_error(k=k_artificial, x=centroids, data=artificial_data['X']))
+
+    # KMeans clustering on iris data:
     iris_kmeans = cluster.KMeans(n_clusters=3, init='random', n_init=10, max_iter=100).fit(
         iris_data['X'])
-    print(iris_kmeans.cluster_centers_)
+    centroids = iris_kmeans.cluster_centers_
+    all_centroids['iris_KMeans'].append(centroids)
+    all_scores['iris_KMeans'].append(quantization_error(k=k_iris, x=centroids, data=iris_data['X']))
 
     # PSO clustering on artificial data:
     print(f'PSO artificial')
@@ -147,7 +180,8 @@ for i in range(n_trials):
         alpha1=alpha1,
         alpha2=alpha2
     )
-    np.append(artificial_PSO_results, best)
+    all_centroids['artificial_PSO'].append(best)
+    all_scores['artificial_PSO'].append(quantization_error(k=k_artificial, x=best, data=artificial_data['X']))
 
     # PSO clustering on iris dataset:
     print(f'PSO iris')
@@ -159,8 +193,41 @@ for i in range(n_trials):
         alpha1=alpha1,
         alpha2=alpha2
     )
-    np.append(iris_PSO_results, best)
+    all_centroids['iris_PSO'].append(best)
+    all_scores['iris_PSO'].append(quantization_error(k=k_iris, x=best, data=iris_data['X']))
 
+mean_quantization_KMeans_artificial = np.mean(all_scores['artificial_KMeans'])
+mean_quantization_PSO_artificial = np.mean(all_scores['artificial_PSO'])
+mean_quantization_KMeans_iris = np.mean(all_scores['iris_KMeans'])
+mean_quantization_PSO_iris = np.mean(all_scores['iris_PSO'])
 
-plot_2D_data(iris_data['X'][:, :2], iris_data['y'])
-plot_2D_data(artificial_data['X'][:, :2], artificial_data['y'])
+print(f'Mean Quantization errors over {n_trials} runs:\n')
+print(f'\t\t|\tArtif.\t|\tIris')
+print('--------+-----------+----------')
+print(f'KMeans\t|\t{mean_quantization_KMeans_artificial:.3f}\t|\t{mean_quantization_KMeans_iris:.3f}')
+print(f'PSO\t\t|\t{mean_quantization_PSO_artificial:.3f}\t|\t{mean_quantization_PSO_iris:.3f}')
+
+# plot_2D_data(iris_data['X'][:, :2], iris_data['y'])
+# plot_2D_data(artificial_data['X'][:, :2], artificial_data['y'])
+
+# Plot KMeans iris data:
+centroids = all_centroids['iris_KMeans'][0]
+_, y_hat = get_clusters(k=k_iris, x=centroids, data=iris_data['X'])
+plot_2D_data(X = iris_data['X'], y=iris_data['y'], y_hat=y_hat, k=k_iris, data_name='Iris', method_name='KMeans')
+
+# Plot KMeans artificial data:
+centroids = all_centroids['artificial_KMeans'][0]
+_, y_hat = get_clusters(k=k_artificial, x=centroids, data=artificial_data['X'])
+plot_2D_data(X = artificial_data['X'], y=artificial_data['y'], y_hat=y_hat, k=k_artificial, data_name='Artificial', method_name='KMeans')
+
+# Plot PSO iris data:
+centroids = all_centroids['iris_PSO'][0]
+_, y_hat = get_clusters(k=k_iris, x=centroids, data=iris_data['X'])
+plot_2D_data(X = iris_data['X'], y=iris_data['y'], y_hat=y_hat, k=k_iris, data_name='Iris', method_name='PSO')
+
+# Plot KMeans artificial data:
+centroids = all_centroids['artificial_PSO'][0]
+_, y_hat = get_clusters(k=k_artificial, x=centroids, data=artificial_data['X'])
+plot_2D_data(X = artificial_data['X'], y=artificial_data['y'], y_hat=y_hat, k=k_artificial, data_name='Artificial', method_name='PSO')
+
+#
