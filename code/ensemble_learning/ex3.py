535fe277af68751ed9f4d16eb6100b9a02ce6865
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-04-24 17:08:20 +0200

    Change ceil to floor + 1

--- code/ensemble_learning/ex3.py
+++ code/ensemble_learning/ex3.py
@@ -8,8 +8,8 @@ ws = np.arange(0, 101) / 100
 
 probs = np.zeros(len(ws))
 for i, w in enumerate(ws):
-    probs[i] = sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.8 for i in range(math.ceil(5/(1-w)), 11)]) + \
-               sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.2 for i in range(math.ceil((5-10*w)/(1-w)), 11)])  # TODO: fix calculation with weight w
+    probs[i] = sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.8 for i in range(math.floor(5/(1-w)), 11 + 1)]) + \
+               sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.2 for i in range(math.floor((5-10*w)/(1-w) + 1), 11)])  # TODO: fix calculation with weight w
 plt.plot(ws, probs)
 
 plt.title(f"Probability of correct diagnosis with majority vote\n"
