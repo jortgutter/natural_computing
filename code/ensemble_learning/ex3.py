39fba59980a2fa78a4bfda6e36b4eae30fd0a977
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-04-24 17:06:48 +0200

    Do something with the weight that doesn't work

--- code/ensemble_learning/ex3.py
+++ code/ensemble_learning/ex3.py
@@ -4,12 +4,12 @@ from scipy.stats import binom
 import numpy as np
 import math
 
-ws = np.arange(1, 100) / 100
+ws = np.arange(0, 101) / 100
 
 probs = np.zeros(len(ws))
 for i, w in enumerate(ws):
-    probs[i] = sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.8 for i in range(5 * math.ceil(5/(1-w)), 11)]) + \
-               sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.2 for i in range(6 * math.ceil((5-10*w)/(1-w)), 11)])  # TODO: fix calculation with weight w
+    probs[i] = sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.8 for i in range(math.ceil(5/(1-w)), 11)]) + \
+               sum([choose(10, i) * np.power(0.6, i) * np.power(1 - 0.6, 10 - i) * 0.2 for i in range(math.ceil((5-10*w)/(1-w)), 11)])  # TODO: fix calculation with weight w
 plt.plot(ws, probs)
 
 plt.title(f"Probability of correct diagnosis with majority vote\n"
