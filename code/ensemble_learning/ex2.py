e7bb41ad1cea65b9456d678f61c09881588f36a7
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-04-24 17:01:42 +0200

    Fix y axis label

--- code/ensemble_learning/ex2.py
+++ code/ensemble_learning/ex2.py
@@ -18,8 +18,7 @@ for p in ps:
 plt.title(f"Probability of correct diagnosis with majority vote\n"
           f"for different values for c (group size) and p (prob. of individual being correct)")
 plt.xlabel("c")
-plt.ylabel("p")
+plt.ylabel("p(correct)")
 plt.xticks(range(0, c_max, 5))
-plt.yticks(ps)
 plt.legend()
 plt.show()
