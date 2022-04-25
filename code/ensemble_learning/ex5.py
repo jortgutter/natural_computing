717871db59ecdd3aaab739b6b70eaed43818dd6e
Jort.Gutter <jort.gutter@ru.nl>
2022-04-25 23:27:51 +0200

    final tweaks ex5

--- code/ensemble_learning/ex5.py
+++ code/ensemble_learning/ex5.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix, f1_score
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
@@ -18,6 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 N_RUNS = 10
 max_trees = 30
 depth_steps = 5
+baseline_depths = 3
 f_scores = np.zeros((depth_steps, N_RUNS, max_trees))
 
 # random forests
@@ -26,7 +27,11 @@ for depth in range(5):
     for i in tqdm(range(N_RUNS)):
         f_scores_run = []
         for n_trees in range(max_trees):
-            forest = RandomForestClassifier(n_estimators=n_trees+1, max_depth = depth+1, random_state=42)
+            forest = RandomForestClassifier(
+                n_estimators=n_trees+1,
+                max_depth = depth+1,
+                random_state=42+i
+            )
             forest.fit(X_train, y_train)
             y_hat_forest = forest.predict(X_test)
             C_forest = confusion_matrix(y_test, y_hat_forest)
@@ -35,9 +40,9 @@ for depth in range(5):
 
 # Decision Tree baseline
 f_scores_baselines = np.zeros((5, 10))
-for i in range(2):
+for i in range(baseline_depths):
     for run in range(N_RUNS):
-        tree = DecisionTreeClassifier(max_depth=(i+1)*5, random_state=42)
+        tree = DecisionTreeClassifier(max_depth=(i+1)*5, random_state=42+i)
         tree.fit(X_train, y_train)
         y_hat_tree = tree.predict(X_test)
         C_tree = confusion_matrix(y_test, y_hat_tree)
@@ -47,12 +52,12 @@ for i in range(2):
 
 # plotting
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
-styles = ['-', '--']
+styles = ['-.', '--', '-', ':']
 for i in range(5):
     #plt.plot(f_scores[i].T, alpha=0.2, color=colors[i])
-    plt.plot(np.mean(f_scores[i], axis=0), color=colors[i], linewidth=2, label=f'Forest, depth={i+1}')
-for j in range(2):
-    plt.hlines(y=np.mean(f_scores_baselines[j], axis=0), xmin=0, xmax=max_trees, color='black', linestyles=styles[j], label=f'Single tree, depth={(j+1)*5}')
+    plt.plot(range(1,31), np.mean(f_scores[i], axis=0), color=colors[i], linewidth=2, label=f'Forest, depth={i+1}')
+for j in range(baseline_depths):
+    plt.hlines(y=np.mean(f_scores_baselines[j], axis=0), xmin=1, xmax=max_trees, color='black', linestyles=styles[j], label=f'Single tree, depth={(j+1)*5}')
 plt.legend()
 plt.xlabel('Number of trees in forest')
 plt.ylabel('F1_score')
