20b6885b0c8e94bc326291086c2d5eae4c44a0c1
Jort.Gutter <jort.gutter@ru.nl>
2022-05-11 14:07:30 +0200

    created network stub

--- project/code/main.py
+++ project/code/main.py
@@ -1,13 +1,24 @@
 import numpy as np
-import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
 
 
 def load_data():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = datasets.fashion_mnist.load_data()
     print(len(x_train))
 
+def network():
+    network = models.Sequential([
+        layers.Conv2D(32, (3,3), activation='relu'),
+        layers.Conv2D()
+    ])
+
+    return network
+
 def main():
+    x = np.array([])
     load_data()
+
 
 if __name__ == "__main__":
     main()
