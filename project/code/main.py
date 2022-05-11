02dd490e71c604e3dc6100a6e916b87a5d90c434
Jort.Gutter <jort.gutter@ru.nl>
2022-05-11 13:27:22 +0200

    load data

--- project/code/main.py
+++ project/code/main.py
@@ -1,7 +1,13 @@
 import numpy as np
+import tensorflow as tf
+
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    print(len(x_train))
 
 def main():
-    print("Hello World!")
+    load_data()
 
 if __name__ == "__main__":
     main()
