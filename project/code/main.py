ae776734618dc9cab9f434c45058cde439ac413d
Evert-Pano <evertbraal@hotmail.nl>
2022-05-19 15:33:40 +0200

    Added plotting during training

--- project/code/main.py
+++ project/code/main.py
@@ -1,7 +1,50 @@
 import numpy as np
-import tensorflow as tf
+import keras
+from matplotlib import pyplot as plt
 from keras.utils.np_utils import to_categorical
 from tensorflow.keras import datasets, layers, models, optimizers
+from IPython.display import clear_output
+
+class PlotLearning(keras.callbacks.Callback):
+    """
+    Callback to plot the learning curves of the model during training.
+    """
+
+    def on_train_begin(self, logs={}):
+        self.metrics = {}
+        for metric in logs:
+            self.metrics[metric] = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        # Storing metrics
+        for metric in logs:
+            if metric in self.metrics:
+                self.metrics[metric].append(logs.get(metric))
+            else:
+                self.metrics[metric] = [logs.get(metric)]
+
+        # Plotting
+        metrics = [x for x in logs if 'val' not in x]
+
+        f, axs = plt.subplots(1, len(metrics), figsize=(15, 5))
+        clear_output(wait=True)
+
+        for i, metric in enumerate(metrics):
+            axs[i].plot(range(1, epoch + 2),
+                        self.metrics[metric],
+                        label=metric)
+            if logs['val_' + metric]:
+                axs[i].plot(range(1, epoch + 2),
+                            self.metrics['val_' + metric],
+                            label='val_' + metric)
+
+            axs[i].legend()
+            axs[i].grid()
+
+        plt.tight_layout()
+        plt.show()
+
+callbacks_list = [PlotLearning()]
 
 def load_data():
     (x_train, y_train), (x_test, y_test) = datasets.fashion_mnist.load_data()
@@ -34,7 +77,7 @@ def define_model():
 
 def train_model(x_train, y_train, x_test, y_test):
     model = define_model()
-    history = model.fit(x_train, y_train, epochs=10, verbose=1, validation_split=0.2)
+    history = model.fit(x_train, y_train, epochs=10, verbose=1, validation_split=0.2, callbacks=callbacks_list)
 
     print("=================================================================")
     _, acc = model.evaluate(x_test, y_test, verbose=1)
