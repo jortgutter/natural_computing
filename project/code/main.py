f5a36f97f760224c4432e8c00a72a1b457b2491a
Evert-Pano <evertbraal@hotmail.nl>
2022-05-19 15:03:03 +0200

    Initial base CNN

--- project/code/main.py
+++ project/code/main.py
@@ -1,23 +1,48 @@
 import numpy as np
-from tensorflow.keras import datasets, layers, models
-
-
+import tensorflow as tf
+from keras.utils.np_utils import to_categorical
+from tensorflow.keras import datasets, layers, models, optimizers
 
 def load_data():
     (x_train, y_train), (x_test, y_test) = datasets.fashion_mnist.load_data()
-    print(len(x_train))
 
-def network():
+    # normalize pixel values to be between 0 and 1
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+    x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    return x_train, y_train, x_test, y_test
+
+def define_model():
     network = models.Sequential([
-        layers.Conv2D(32, (3,3), activation='relu'),
-        layers.Conv2D()
+        layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D((2,2)),
+        layers.Conv2D(64, (3,3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(10, activation='softmax')
     ])
-
+    optimizer = optimizers.SGD(learning_rate=0.01, momentum=0.9)
+    network.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    print(network.summary())
     return network
 
+def train_model(x_train, y_train, x_test, y_test):
+    model = define_model()
+    model.fit(x_train, y_train, epochs=10, verbose=1)
+    print("=================================================================")
+    _, acc = model.evaluate(x_test, y_test, verbose=1)
+
+
+
+
 def main():
-    x = np.array([])
-    load_data()
+    x_train, y_train, x_test, y_test = load_data()
+    train_model(x_train, y_train, x_test, y_test)
+
 
 
 if __name__ == "__main__":
